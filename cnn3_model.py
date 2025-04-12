e616b87757df403efcb5a9521fd1afd80a1dce12
Anna Feleki <felekiann@gmail.com>
2025-04-12 09:19:32 +0300

    Update cnn3_model.py

--- cnn3_model.py
+++ cnn3_model.py
@@ -1,71 +1,45 @@
-import matplotlib.pyplot as plt
-def plot_roc_curve(fpr, tpr):
-    plt.plot(fpr, tpr, color='orange', label='ROC')
-    plt.plot([0, 1], [0, 1], color='darkblue', linestyle='--')
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic (ROC) Curve')
-    plt.legend()
-    plt.show()
-
-import os
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import glob
-import time
-import keras
-
-def read_and_process_image():
-  ...
-  
-  
-  
-# Select number of channels
-channels = 3  # 1 grayscale, 3 rgb
-# Create the X, y datasets for the ML processing
-X,y = read_and_process_image(addrs)
- 
-X = np.array(X)
-y = np.array(y)
- 
-print(X.shape)
- 
-
-from keras import utils as np_utils
-y = keras.utils.np_utils.to_categorical(y, num_classes=classes_num)
-print(y.shape)
-
- 
-# Split the dataset
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv3D, MaxPooling3D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.optimizers import Adam
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.20, random_state=42)
- 
-# Get the length of the train and validation data
-ntrain = len(X_train)
-nval = len(X_val)
-ntest = len(X_test)
-print("X train length:", len(X_train))
-print("X validation length:", len(X_val))
-print("X test length:", len(X_test))
- 
-# Import CNN-related libraries
-import keras
-from keras import layers
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
- 
- 
-# Batch size (should be a factor of 2.***4,8,16,32,64...***)
-batch_size = 32
-# Dropout rate
-drop_rate = 0.2
-# Number of epochs
-num_epochs = 500
- 
-# Initiate and build the model
-model = Sequential()
-....
 
+# Simulated 3D image dataset
+X = np.random.rand(200, 32, 32, 32, 1)  # 200 samples, 32x32x32 voxel images, 1 channel
+y = np.random.randint(0, 2, 200)       # Binary classification
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Convert labels to categorical
+y_train = tf.keras.utils.to_categorical(y_train, 2)
+y_test = tf.keras.utils.to_categorical(y_test, 2)
+
+# Build 3D CNN model
+model = Sequential()
+model.add(Conv3D(32, kernel_size=(3, 3, 3), activation='relu', input_shape=(32, 32, 32, 1)))
+model.add(BatchNormalization())
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+
+model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+# Compile
+model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train
+history = model.fit(X_train, y_train, validation_split=0.2, epochs=15, batch_size=8)
+
+# Evaluate
+loss, accuracy = model.evaluate(X_test, y_test)
+print(f"Test Accuracy: {accuracy:.2f}")
+
+# Save model
+model.save("3d_cnn_model.h5")
